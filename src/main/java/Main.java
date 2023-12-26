42f9ca1df1e10766203b7840c63ee879467700fb
LazyStorage <154854665+LazyStorage@users.noreply.github.com>
2023-12-26 11:41:09 +0800

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Represents the main entry point of the app.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void testDummy() {
+        assertTrue(true);
+    }
+}
